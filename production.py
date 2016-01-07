26fc97257b793797a7e19fb39602e87489624ccd
Prakash Pandey <prakash.pandey@fulfil.io>
2016-01-07 19:58:54 +0530

    Module is maintained by fulfil.io

--- production.py
+++ production.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-    production_disassembly.py
-
-    :copyright: (c) 2014 by Openlabs Technologies & Consulting (P) Limited
-    :license: BSD, see LICENSE for more details.
-"""
 from decimal import Decimal
 from collections import namedtuple
 
